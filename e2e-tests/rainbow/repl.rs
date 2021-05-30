2b462e41ad29e46053d56c8d08370ba315d23a22
Alex Ostrovski <ostrovski.alex@gmail.com>
2021-05-30 22:00:22 +0300

    Add REPL binary to test crate

---
+++ e2e-tests/rainbow/repl.rs
@@ -0,0 +1,98 @@
+//! Simple REPL application that echoes the input with coloring / styles applied.
+
+use rustyline::{error::ReadlineError, Editor};
+use termcolor::{Ansi, Color, ColorSpec, WriteColor};
+
+use std::io;
+
+use term_transcript::svg::RgbColor;
+
+const PROMPT: &str = ">>> ";
+
+fn process_line(writer: &mut impl WriteColor, line: &str) -> io::Result<()> {
+    let parts: Vec<_> = line.split_whitespace().collect();
+    let mut color_spec = ColorSpec::new();
+    for (i, &part) in parts.iter().enumerate() {
+        match part {
+            "bold" => {
+                color_spec.set_bold(true);
+            }
+            "italic" => {
+                color_spec.set_italic(true);
+            }
+            "underline" => {
+                color_spec.set_underline(true);
+            }
+            "intense" => {
+                color_spec.set_intense(true);
+            }
+
+            "black" => {
+                color_spec.set_fg(Some(Color::Black));
+            }
+            "blue" => {
+                color_spec.set_fg(Some(Color::Blue));
+            }
+            "green" => {
+                color_spec.set_fg(Some(Color::Green));
+            }
+            "red" => {
+                color_spec.set_fg(Some(Color::Red));
+            }
+            "cyan" => {
+                color_spec.set_fg(Some(Color::Cyan));
+            }
+            "magenta" => {
+                color_spec.set_fg(Some(Color::Magenta));
+            }
+            "yellow" => {
+                color_spec.set_fg(Some(Color::Yellow));
+            }
+            "white" => {
+                color_spec.set_fg(Some(Color::White));
+            }
+
+            color if color.starts_with('#') => {
+                if let Ok(color) = color.parse::<RgbColor>() {
+                    color_spec.set_fg(Some(Color::Rgb(color.0, color.1, color.2)));
+                }
+            }
+
+            _ => { /* Do nothing. */ }
+        }
+
+        writer.set_color(&color_spec)?;
+        write!(writer, "{}", part)?;
+        writer.reset()?;
+        if i + 1 < parts.len() {
+            write!(writer, " ")?;
+        }
+    }
+    writeln!(writer)
+}
+
+fn main() -> anyhow::Result<()> {
+    let mut rl = Editor::<()>::new();
+    let mut writer = Ansi::new(io::stdout());
+
+    loop {
+        let line = rl.readline(PROMPT);
+        match line {
+            Ok(line) => {
+                process_line(&mut writer, &line)?;
+                rl.add_history_entry(line);
+            }
+
+            Err(ReadlineError::Interrupted) => {
+                println!("Bye");
+                break Ok(());
+            }
+
+            Err(ReadlineError::Eof) => {
+                break Ok(());
+            }
+
+            Err(e) => panic!("Error reading command: {}", e),
+        }
+    }
+}
