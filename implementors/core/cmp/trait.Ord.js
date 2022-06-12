e4a48aeea1cc9717cb31956dcfa391195130158e
slowli <slowli@users.noreply.github.com>
2022-06-12 16:05:38 +0000

    Deploying to gh-pages from  @ 4806810309d4256df13e17b36d7424b97c76b247 🚀

---
+++ implementors/core/cmp/trait.Ord.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["term_transcript"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"term_transcript/test/enum.MatchKind.html\" title=\"enum term_transcript::test::MatchKind\">MatchKind</a>","synthetic":false,"types":["term_transcript::test::MatchKind"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["term_transcript"] = {"name":"","dirs":[{"name":"html","files":["mod.rs"]},{"name":"shell","files":["mod.rs","standard.rs","transcript_impl.rs"]},{"name":"svg","files":["mod.rs","palette.rs"]},{"name":"term","files":["mod.rs","parser.rs"]},{"name":"test","dirs":[{"name":"parser","files":["mod.rs","text.rs"]}],"files":["color_diff.rs","config_impl.rs","mod.rs","utils.rs"]}],"files":["lib.rs","pty.rs","traits.rs","utils.rs"]};
+createSourceSidebar();
