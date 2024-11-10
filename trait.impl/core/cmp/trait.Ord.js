27d2b500c5b412c26ec1051a41ff1eafb74936ca
slowli <slowli@users.noreply.github.com>
2024-11-10 15:40:36 +0000

    Deploying to gh-pages from @ slowli/term-transcript@99198c7a9255f566eaea15b7d84f11ae84527fd7 🚀

---
+++ trait.impl/core/cmp/trait.Ord.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"term_transcript":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"term_transcript/test/enum.MatchKind.html\" title=\"enum term_transcript::test::MatchKind\">MatchKind</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
