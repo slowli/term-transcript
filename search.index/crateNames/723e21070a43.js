328545e52d96748ccc819eea79242e43f7db462b
slowli <slowli@users.noreply.github.com>
2026-01-05 07:32:28 +0000

    Deploying to gh-pages from @ slowli/term-transcript@286feeb8ab580b5bbca8195c530b6babcdd8cbe2 🚀

---
+++ search.index/crateNames/723e21070a43.js
@@ -0,0 +1,1 @@
+rd_("oterm_transcript")
---
+++ trait.impl/core/convert/trait.AsRef.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["term_transcript",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.str.html\">str</a>&gt; for <a class=\"struct\" href=\"term_transcript/struct.Captured.html\" title=\"struct term_transcript::Captured\">Captured</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.str.html\">str</a>&gt; for <a class=\"struct\" href=\"term_transcript/struct.UserInput.html\" title=\"struct term_transcript::UserInput\">UserInput</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[780]}
