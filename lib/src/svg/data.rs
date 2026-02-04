c0e507e8cd0a980aad3af38040f4394c3499515b
Alex Ostrovski <ostrovski.alex@gmail.com>
2026-02-04 18:01:34 +0200

    Update version mentions in snapshots

--- lib/src/svg/data.rs
+++ lib/src/svg/data.rs
@@ -32,7 +32,7 @@ use crate::{
 /// let expected_json = serde_json::json!({
 ///     "creator": {
 ///         "name": "term-transcript",
-///         "version": "0.4.0",
+///         "version": "0.5.0-beta.1",
 ///         "repo": "https://github.com/slowli/term-transcript",
 ///     },
 ///     "width": 720,
